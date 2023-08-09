76e9fdd314aa51f5ee778e19079c6c553eb95c57
Akhil Nandyala <akhilpie3@gmail.com>
2023-08-09 09:23:19 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+// src/components/PostList.js
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [newPost, setNewPost] = useState({ title: "", body: "" });
+  const [notification, setNotification] = useState(null);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => setPosts(response.data))
+      .catch((error) => console.error("Error fetching data:", error));
+  };
+
+  const addPost = () => {
+    axios
+      .post("https://jsonplaceholder.typicode.com/posts", newPost)
+      .then((response) => {
+        console.log("New post added:", response.data);
+        fetchPosts();
+        setNotification("Post added successfully!");
+        setNewPost({ title: "", body: "" }); // Clear the input fields
+        setTimeout(() => setNotification(null), 3000);
+      })
+      .catch((error) => console.error("Error adding post:", error));
+  };
+
+  const updatePost = (postId, updatedPost) => {
+    axios
+      .put(`https://jsonplaceholder.typicode.com/posts/${postId}`, updatedPost)
+      .then((response) => {
+        console.log("Post updated:", response.data);
+        fetchPosts();
+        setNotification("Post updated successfully!");
+        setTimeout(() => setNotification(null), 3000);
+      })
+      .catch((error) => console.error("Error updating post:", error));
+  };
+
+  const deletePost = (postId) => {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then((response) => {
+        console.log("Post deleted:", response.data);
+        fetchPosts();
+        setNotification("Post deleted successfully!");
+        setTimeout(() => setNotification(null), 3000);
+      })
+      .catch((error) => console.error("Error deleting post:", error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {notification && <div className="notification">{notification}</div>}
+      <h1>Post List</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {post.title}
+            <button
+              onClick={() => updatePost(post.id, { title: "Updated Title" })}
+            >
+              Update
+            </button>
+            <button onClick={() => deletePost(post.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <h2>Add New Post</h2>
+      <input
+        type="text"
+        placeholder="Title"
+        value={newPost.title}
+        onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
+      />
+      <textarea
+        placeholder="Body"
+        value={newPost.body}
+        onChange={(e) => setNewPost({ ...newPost, body: e.target.value })}
+      />
+      <button onClick={addPost}>Add Post</button>
     </div>
   );
 }
